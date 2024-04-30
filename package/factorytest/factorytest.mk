65994600db55ec0db7a70a138f63a10785a3e7a1
James Deng <james.deng@spacemit.com>
2024-04-30 17:48:07 +0800

    Update for v1.0rc1

--- package/factorytest/factorytest.mk
+++ package/factorytest/factorytest.mk
@@ -3,7 +3,13 @@ FACTORYTEST_SITE = $(BR2_EXTERNAL_Bianbu_PATH)/../package-src/factorytest
 FACTORYTEST_SITE = local
 
 define FACTORYTEST_INSTALL_TARGET_CMDS
-	cp -r $(@D) $(TARGET_DIR)/opt/factorytest
+	rm -rf $(TARGET_DIR)/opt/factorytest
+	mkdir -p $(TARGET_DIR)/opt/factorytest
+	cp -r $(@D)/cricket $(TARGET_DIR)/opt/factorytest
+	cp -r $(@D)/res $(TARGET_DIR)/opt/factorytest
+	cp -r $(@D)/tests $(TARGET_DIR)/opt/factorytest
+	cp -r $(@D)/utils $(TARGET_DIR)/opt/factorytest
+	cp $(@D)/gui-main $(TARGET_DIR)/opt/factorytest
 endef
 
 $(eval $(generic-package))
